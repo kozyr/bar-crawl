85902f8a7c9ec2b81ef253103aeb89e360d4d9c5
Sergei <sergei@faspark.com>
2014-01-19 13:57:49 -0600

    Go anync

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,6 +8,8 @@ import models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import play.Logger;
+import play.libs.F;
+import static play.libs.F.Promise.promise;
 import play.libs.Json;
 import play.mvc.Result;
 import repositories.BarCrawlRepository;
@@ -19,6 +21,7 @@ import views.html.index;
 
 import java.util.*;
 
+import static play.mvc.Results.async;
 import static play.mvc.Results.notFound;
 import static play.mvc.Results.ok;
 
@@ -40,22 +43,34 @@ public class Application {
         return ok(index.render());
     }
 
-    public Result barCrawl(double lat, double lon) {
-        Stopwatch watch = new Stopwatch();
-        watch.start();
+    public F.Promise<Result> barCrawl(final double lat, final double lon) {
+        F.Promise<Optional<Path>> barPromise = F.Promise.promise(
+                new F.Function0<Optional<Path>>() {
+                    public Optional<Path> apply() {
+                        return calcBarPath(lat, lon);
+                    }
+                }
+        );
+        return barPromise.map(
+                new F.Function<Optional<Path>, Result>() {
+                    public Result apply(Optional<Path> path) {
+                        if (path.isPresent()) {
+                            return ok(Json.toJson(path.get()));
+                        } else {
+                            return notFound();
+                        }
+                    }
+                }
+        );
+    }
 
+    private Optional<Path> calcBarPath(double lat, double lon) {
         GeoPoint start = new GeoPoint(lat, lon);
         Zone zone = zoneService.buildZone(start, 2* MAX_BAR_CRAWL_DISTANCE);
         List<DirectedBlock> first = routeProvider.getBlockFromPoint(start, zone);
         Logger.info("Initial streets: "  + first);
         Optional<Path> path = tourGuide.ratingBfs(first, zone, MAX_BAR_CRAWL_DISTANCE);
 
-        watch.stop();
-        Logger.info("Total time: " + watch);
-        if (path.isPresent()) {
-            return ok(Json.toJson(path.get()));
-        } else {
-            return notFound();
-        }
+        return path;
     }
 }
--- app/services/ZoneService.java
+++ app/services/ZoneService.java
@@ -1,5 +1,6 @@
 package services;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Stopwatch;
 import models.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import play.Logger;
+import play.libs.F;
 import repositories.StreetRepository;
+import scala.concurrent.impl.Future;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ZoneService {
@@ -19,30 +23,31 @@ public class ZoneService {
     @Autowired
     private BarService barService;
 
-    public Zone buildZone(GeoPoint center, int distance) {
-        Stopwatch watch = new Stopwatch();
-        watch.start();
+    public Zone buildZone(final GeoPoint center, final int distance) {
+        F.Promise<List<Street>> streetPromise = F.Promise.promise(
+                new F.Function0<List<Street>>() {
+                    public List<Street> apply() {
+                        return streetRepository.findByLocation(center.getGeom(), distance);
+                    }
+                }
+        );
 
-        List<Street> streets = streetRepository.findByLocation(center.getGeom(), distance);
-        /*if (Logger.isDebugEnabled()) {
-            for (Street s : streets) {
-                Logger.debug(s.toString());
+        F.Promise<List<Bar>> barPromise = F.Promise.promise(
+                new F.Function0<List<Bar>>() {
+                    public List<Bar> apply() {
+                        return barService.findByLocation(center, distance);
+                    }
+                }
+        );
+
+        F.Promise<Zone> result = F.Promise.sequence(streetPromise, barPromise).map(new F.Function<List<List<? extends Object>>, Zone>() {
+            @Override
+            public Zone apply(List<List<? extends Object>> lists) throws Throwable {
+                return new Zone((List<Street>) lists.get(0), (List<Bar>) lists.get(1));
             }
-        }*/
-        List<Bar> bars = barService.findByLocation(center, distance);
-        /*if (Logger.isDebugEnabled()) {
-            for (Bar b : bars) {
-                Logger.debug(b.toString());
-            }
+        });
 
-        }*/
-
-        watch.stop();
-        Logger.info("Time to read in data: " + watch);
-
-        Zone zone = new Zone(streets, bars);
-
-        return zone;
+        return result.get(10, TimeUnit.SECONDS);
     }
 
 }
