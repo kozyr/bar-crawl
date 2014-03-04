a2d43b37c69479264831f6360bf146748922a83e
sergei <sergei@parknav.com>
2014-03-03 18:57:08 -0600

    Add first test

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,92 +1,54 @@
 import org.junit.Test;
-import play.data.Form;
 import play.libs.WS;
-import play.mvc.Content;
 import play.mvc.Result;
-import play.test.FakeRequest;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.*;
 
 public class ApplicationTest {
 
-    /*
-    @Test
-    public void indexTemplate() {
-        running(fakeApplication(), new Runnable() {
-            public void run() {
-                Form<Place> form = Form.form(Place.class);
-                Content html = views.html.index.render(form);
-                assertThat(contentType(html)).isEqualTo("text/html");
-                assertThat(contentAsString(html)).contains("Welcome");
-            }
-        });
-    }
+    private static final double TEST_LAT = 41.944632;
+    private static final double TEST_LON = -87.654455;
 
     @Test
     public void callIndex() {
-        Result result = callAction(controllers.routes.ref.Application.index());
-        assertThat(status(result)).isEqualTo(OK);
-        assertThat(contentType(result)).isEqualTo("text/html");
-        assertThat(charset(result)).isEqualTo("utf-8");
-        assertThat(contentAsString(result)).contains("Welcome");
-    }
-
-    @Test
-    public void callAddBar() {
         running(fakeApplication(), new Runnable() {
             public void run() {
-                Map<String, String> formParams = new HashMap<String, String>();
-                formParams.put("name", "foo");
-                
-                FakeRequest fakeRequest = fakeRequest().withFormUrlEncodedBody(formParams);
-                
-                Result result = callAction(controllers.routes.ref.Application.addBar(), fakeRequest);
-                assertThat(status(result)).isEqualTo(SEE_OTHER);
+                Result result = callAction(controllers.routes.ref.Application.index());
+                assertThat(status(result)).isEqualTo(OK);
+                assertThat(contentType(result)).isEqualTo("text/html");
+                assertThat(charset(result)).isEqualTo("utf-8");
+                assertThat(contentAsString(result)).contains("Place Crawl App");
             }
         });
     }
 
     @Test
-    public void callListBars() {
+    public void callPlaceBrawl() {
         running(fakeApplication(), new Runnable() {
             public void run() {
-                Map<String, String> formParams = new HashMap<String, String>();
-                formParams.put("name", "foo");
-
-                FakeRequest fakeRequest = fakeRequest().withFormUrlEncodedBody(formParams);
-
-                callAction(controllers.routes.ref.Application.addBar(), fakeRequest);
-                
-                Result result = callAction(controllers.routes.ref.Application.listBars());
+                Result result = callAction(controllers.routes.ref.Application.placeCrawl(TEST_LAT, TEST_LON));
                 assertThat(status(result)).isEqualTo(OK);
                 assertThat(contentType(result)).isEqualTo("application/json");
-                assertThat(contentAsString(result)).startsWith("[");
-                assertThat(contentAsString(result)).contains("foo");
+                String content = contentAsString(result);
+                assertThat(content).startsWith("{");
+                assertThat(content).contains("osm_id");
             }
         });
     }
 
-    @Test
-    public void barsRoute() {
-        running(fakeApplication(), new Runnable() {
-            public void run() {
-                Result result = route(fakeRequest(GET, "/bars"));
-                assertThat(result).isNotNull();
-            }
-        });
-    }
 
     @Test
-    public void realBarsRequest() {
+    public void realCrawlRequest() {
         running(testServer(3333), new Runnable() {
             public void run() {
-                assertThat(WS.url("http://localhost:3333/bars").get().get().getStatus()).isEqualTo(OK);
+                assertThat(WS.url("http://localhost:3333/place-crawl").
+                        setQueryParameter("lat", String.valueOf(TEST_LAT)).
+                        setQueryParameter("lon", String.valueOf(TEST_LON)).
+                        get().get(10, TimeUnit.SECONDS).getStatus()).isEqualTo(OK);
             }
         });
     }
-    */
 }
